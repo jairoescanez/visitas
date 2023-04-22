95cc1c16ce010c576fc57c654bf511aa707a93fe
nicmarvan <nicmarvan@alum.us.es>
2023-04-22 14:57:24 +0200

    Refinado formulario y activado calendario

--- models/habitacion.py
+++ models/habitacion.py
@@ -5,7 +5,7 @@ class Habitacion(models.Model):
     _name = "habitacion"
     _description = "Habitaciones"
 
-    num_habitacion = fields.Integer(required=True)
-    precio = fields.Float(required=True)
-    num_adultos = fields.Integer(required=True)
-    num_ninos = fields.Integer(required=True)
+    num_habitacion = fields.Integer(required=True, string="Número de habitación")
+    precio = fields.Float(required=True, string="Precio")
+    num_adultos = fields.Integer(required=True, string="Número de adultos")
+    num_ninos = fields.Integer(required=True, string="Número de niños")
--- models/visita.py
+++ models/visita.py
@@ -7,17 +7,24 @@ class Visita(models.Model):
     _name = "visita"
     _description = "Gestor de visitas"
 
-    fecha_entrada = fields.Datetime(required=True, default=datetime.now())
-    fecha_salida = fields.Datetime(required=True)
-    nombre_reserva = fields.Char(required=True)
-    dni = fields.Char(required=True)
-    numero_telefono = fields.Char(required=True)
-    es_reserva = fields.Boolean(required=True, default=False)
-    habitacion = fields.Many2one(required=True, comodel_name="habitacion")
-    persona_visitada = fields.Char()
+    fecha_entrada = fields.Datetime(required=True, default=datetime.now(), string="Fecha y hora de entrada")
+    fecha_salida = fields.Datetime(required=True, string="Fecha y hora de salida")
+    nombre_reserva = fields.Char(required=True, string="Nombre y Apellidos")
+    dni = fields.Char(required=True, string="DNI")
+    numero_telefono = fields.Char(required=True, string="Teléfono")
+    es_reserva = fields.Boolean(required=True, default=False, string="Reserva habitación")
+    habitacion = fields.Many2one(comodel_name="habitacion", string="Habitación")
+    persona_visitada = fields.Char(string="Residente Visitado")
 
     @api.constrains("fecha_entrada","fecha_salida")
     def _check_fecha_entrada_anterior_fecha_salida(self):
         for p in self:
             if (p.fecha_entrada > p.fecha_salida):
                 raise ValidationError("La fecha de entrada debe ser anterior a la fecha de salida")
+
+    @api.onchange("es_reserva")
+    def onchange(self):
+        if self.es_reserva == True:
+            self.persona_visitada = None
+        else:
+            self.habitacion = None
